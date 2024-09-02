65ddf6b5172a0305d5d9c099a201fe13efd97cc0
lean679/lean679 <lj.bonillag@uea.edu.ec>
2024-09-01 19:45:18 -0500

    1 de octubre repositorio 1

---
+++ 3.3 .2 tarea 11.py
@@ -0,0 +1,20 @@
+# Matriz de 3x3
+matriz :[
+ [6,8,3]
+ [8,4,3]
+ [2,7,1]
+    ]
+
+print(matriz)
+#funcon buscar_valor especifico
+print(len(matriz))
+ #def buscar_valor(matriz,valor_buscado):
+ for i in range((len(matriz)):
+     for j in range(len(matriz)):
+         if matriz [i][j]==valor_buscado:
+             return i,j
+
+ valor_buscado = 6
+ print (“valor encontrado” ,buscar_valor(matriz,valor_buscado)
+else:
+   print (“valor incorrecto”)
